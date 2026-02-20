b0a4cfdb6dd029b49c33488d1d03d38f9f9c3244
Che' J. Holloway <chejholloway@outlook.com>
2026-02-20 12:33:38 -0800

    fix(trpc): expose profile router in JS build for in-repo tests

--- server/trpc/router.js
+++ server/trpc/router.js
@@ -1,9 +1,11 @@
-import { router } from "./base";
-import { ogRouter } from "./routers/og";
-import { postRouter } from "./routers/post";
-import { authRouter } from "./routers/auth";
+import { router } from './base';
+import { ogRouter } from './routers/og';
+import { postRouter } from './routers/post';
+import { authRouter } from './routers/auth';
+import { profileRouter } from './routers/profile';
 export const appRouter = router({
-    og: ogRouter,
-    post: postRouter,
-    auth: authRouter
+  og: ogRouter,
+  post: postRouter,
+  auth: authRouter,
+  profile: profileRouter,
 });
